ba8318b73c7828f5849d00c62074cc2be803cbff
Dennis Periquet <dperique@redhat.com>
2024-06-16 19:39:53 -0400

    cors fixed

--- ui_be_same_container/ui2/ui.js
+++ ui_be_same_container/ui2/ui.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
+
 const app = express();
 const port = 7007;
+
+// Configure CORS options
+const corsOptions = {
+    origin: '*', // Allow all origins (adjust this in a production environment)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
 
 // Middleware to serve static files from 'public' directory
 app.use(express.static('public'));
